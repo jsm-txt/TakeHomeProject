6b693e5ca1ceca0ae7803b8413328d22a53a8284
jsm-txt <hose153@gmail.com>
2021-06-14 16:24:56 -0500

    Getting the api working

--- app.py
+++ app.py
@@ -12,7 +12,6 @@ app = Flask(__name__)
 load_dotenv()
 API_KEY = os.getenv('API_KEY')
 
-
 my_loader = jinja2.ChoiceLoader([
     app.jinja_loader,
     jinja2.FileSystemLoader('data'),
@@ -34,7 +33,38 @@ def home():
     }
     return render_template('home.html', **context)
 
+def get_units(units):
+    return 'F' if units == 'imperial' else 'C' 
 
+@app.route('/results')
+def results():
+    city = request.args.get('city')
+    units = request.args.get('units')
+
+    url = 'http://api.openweathermap.org/data/2.5/weather'
+    params = {
+        'q': city,
+  
+        'appid': API_KEY
+    }
+
+    result_json = requests.get(url, params=params).json()
+
+    pp.pprint(result_json)
+
+    context = {
+        'date': datetime.now(),
+        'city': city,
+        'description': result_json['weather'][0]['description'],
+        'temp': result_json['main']['temp'],
+        'humidity': result_json['main']['humidity'],
+        'wind_speed': result_json['wind']['speed'],
+        'sunrise': result_json['sys']['sunrise'],
+        'sunset': result_json['sys']['sunset'],
+        'units_letter': get_units(units)
+    }
+
+    return render_template('results.html', **context)
 
 if __name__ == '__main__':
     app.run(debug=True)
